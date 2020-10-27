bb05b399a87fd81f018c8a8d91438399e624e7cf
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2020-10-27 17:26:24 +0100

    spa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './styles/App.scss';
+import { Switch, Route } from 'react-router-dom';
 
+import { Home } from './pages/Home';
+import { ProductDetails } from './pages/ProductDetails';
+import { NotFound } from './pages/NotFound';
+import { Header } from './component/Header';
 
 function App() {
   return (
     <div>
+      <Header>Tienda de móbiles</Header>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/detail/:mobileId' component={ProductDetails} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
