9aa5632057dd3bc2df3371526f0af6417f24fe55
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2020-10-30 16:08:27 +0100

    refactor request service

--- src/services/addProducts.js
+++ src/services/addProducts.js
@@ -1,14 +1,18 @@
 const addCart = 'https://front-test-api.herokuapp.com/api/cart';
 
-export const addProducts = (add) => {
-  return fetch(addCart, {
+export const addProducts = async (add) => {
+  const config = {
     method: 'POST',
     body: JSON.stringify(add),
     headers: {
       'Content-Type': 'application/json',
     },
-  })
-    .then((response) => response.json())
-    .then((response) => response)
-    .catch((error) => error);
+  };
+  try {
+    const response = await fetch(addCart, config);
+    const productCart = await response.json();
+    return productCart;
+  } catch (error) {
+    throw error;
+  }
 };
--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,15 +1,21 @@
 const endpointProducts = 'https://front-test-api.herokuapp.com/api/product/';
 
 export const getProductList = async () => {
-  return await fetch(endpointProducts)
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => error);
+  try {
+    let response = await fetch(endpointProducts);
+    let data = await response.json();
+    return data.map((productList) => productList);
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getProductId = async (id) => {
-  return await fetch(endpointProducts + id)
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => error);
+  try {
+    let response = await fetch(endpointProducts + id);
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
