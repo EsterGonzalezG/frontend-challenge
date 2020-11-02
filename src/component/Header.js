8727470bf25a3b166e2faa9f98dd2450b89fa7e0
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2020-11-02 12:01:19 +0100

    added icons

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,22 @@
 import { PropTypes } from 'prop-types';
 import React from 'react';
+import { SiDsautomobiles } from 'react-icons/si';
 import { Breadcrumbs } from './Breadcrumbs';
 import { ItemCard } from './ItemCart';
 import { Title } from './Title';
 
 export const Header = ({ cart }) => (
   <div className='header'>
-    <div className='l-marginBottom-24'>
-      <div className='header-content'>
-        <Title>Tienda de móbiles</Title>
-        <ItemCard cart={cart} />
+    <div className='header-content'>
+      <div className='l-flexAlignCenter l-marginBottom-16'>
+        <span className='header-icon'>
+          <SiDsautomobiles />
+        </span>
+        <Title>ShopMobile</Title>
       </div>
+      <Breadcrumbs />
     </div>
-    <Breadcrumbs />
+    <ItemCard cart={cart} />
   </div>
 );
 
--- src/component/ItemCart.js
+++ src/component/ItemCart.js
@@ -1,9 +1,13 @@
 import { PropTypes } from 'prop-types';
 import React from 'react';
+import { BiCartAlt } from 'react-icons/bi';
 
 export const ItemCard = ({ cart }) => (
-  <div className='header-itemCart'>
-    <p>{cart}</p>
+  <div className='itemCart l-flexCenterAround'>
+    <span className='itemCart-icon'>
+      <BiCartAlt />
+    </span>
+    <p className='itemCart-number' data-number={cart}></p>
   </div>
 );
 ItemCard.prototype = {
