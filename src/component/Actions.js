6ff8d69840104aebd73693a431961ae672a25f03
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2020-11-02 11:58:30 +0100

    refactor description component

--- src/component/Actions.js
+++ src/component/Actions.js
@@ -1,22 +1,28 @@
 import { PropTypes } from 'prop-types';
 import React, { useEffect } from 'react';
+import { ButtonBackToHome } from './ButtonBackToHome';
 import { Select } from './Select';
 
 export const Actions = ({ product, setColor, setStorage, addProductCart }) => {
   useEffect(() => {
-    if (Object.keys(product).length) {
+    if (Object.entries(product).length) {
       setColor(product?.options?.colors[0].code);
       setStorage(product?.options?.storages[0].code);
     }
   }, [setColor, setStorage, product]);
 
   return (
-    <form>
-      <Select options={product?.options?.colors} name='Color' setOptionsValue={setColor} />
-      <Select options={product?.options?.storages} name='Almacenamiento' setOptionsValue={setStorage} />
-      <button type='button' onClick={addProductCart}>
-        Añadir
-      </button>
+    <form className='actions'>
+      <label className='actions-label l-flexCenterAround l-marginBottom-24'>
+        <Select options={product?.options?.colors} name='Color' setOptionsValue={setColor} />
+        <Select options={product?.options?.storages} name='Almacenamiento' setOptionsValue={setStorage} />
+      </label>
+      <div className='l-flexCenterAround'>
+        <button type='button' onClick={addProductCart} className='Button Button--primary'>
+          Añadir
+        </button>
+        <ButtonBackToHome>Volver</ButtonBackToHome>
+      </div>
     </form>
   );
 };
